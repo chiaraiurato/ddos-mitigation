f6580e3962cb95790e19efaff26f7504e341f943
Alessandro Cortese <74416238+alessandro-cortese@users.noreply.github.com>
2025-08-02 09:26:19 +0200

    Update
    Update after restoring Ubuntu...
    Everything is working fine.

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/alessandro/Documenti/GitHub/ddos-mitigation/.venv
+set -gx VIRTUAL_ENV /home/ale/Documenti/GitHub/ddos-mitigation/.venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
